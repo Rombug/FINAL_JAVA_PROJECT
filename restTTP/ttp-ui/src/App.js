58360a46c1ee13f7a93808573cca0d4dd8948dea
Rombug <119162581+Rombug@users.noreply.github.com>
2023-05-28 16:52:30 +0300

    FINAL PROJECT. (Spring + React) Created TTP skeleton.

--- restTTP/ttp-ui/src/App.js
+++ restTTP/ttp-ui/src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/header/Header";
+import Content from "./components/content/Content";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div> Hello </div>
+    <>
+        <Header/>
+        <Content/>
+        <Footer/>
+    </>
   );
 }
 
