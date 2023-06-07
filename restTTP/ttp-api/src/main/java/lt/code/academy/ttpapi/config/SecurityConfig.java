cf0e55696bb58edaefa51283f0ed2f70d95fc026
Rombug <119162581+Rombug@users.noreply.github.com>
2023-06-07 14:01:05 +0300

    FINAL PROJECT. (Spring + React) added possibility connect to h2 console

--- restTTP/ttp-api/src/main/java/lt/code/academy/ttpapi/config/SecurityConfig.java
+++ restTTP/ttp-api/src/main/java/lt/code/academy/ttpapi/config/SecurityConfig.java
@@ -3,14 +3,15 @@ package lt.code.academy.ttpapi.config;
 import lt.code.academy.ttpapi.security.filter.JwtAuthenticationFilter;
 import lt.code.academy.ttpapi.security.filter.JwtAuthorizationFilter;
 import lt.code.academy.ttpapi.security.service.JwtService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
@@ -37,5 +38,12 @@ public class SecurityConfig {
                     .addFilter(new JwtAuthorizationFilter(authenticationConfiguration.getAuthenticationManager(), jwtService));
 
         return security.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web
+                .ignoring()
+                .requestMatchers(PathRequest.toH2Console());
     }
 }
